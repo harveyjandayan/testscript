a3a1ae527ab555398914dd51616da8a316e40424
harveyjandayan <90567599+harveyjandayan@users.noreply.github.com>
2022-06-14 20:45:57 +0800

    Update relatedlink.js

--- relatedlink.js
+++ relatedlink.js
@@ -1,17 +1,70 @@
+var myPlayer, timeoutID, removeCTATime;
+videojs.getPlayer('myPlayerID').ready(function() {
+  var cuePointAra = [],
+    allCuePointData;
+  myPlayer = this;
+  // Hide overlay initially
+  myPlayer.addClass("hide-overlay");
 
-videojs.registerPlugin('pluginName123', function(options) {
-  var myPlayer = this;
-  
-  // +++ Wait for loadstart to read video information +++
+  // +++ Listen for cue change then pass data to be displayed +++
   myPlayer.on("loadstart", function(evt) {
-    
-    // +++ Read test and link from video info and build anchor tag +++
-    var linkText = myPlayer.mediainfo.link.text,
-      linkURL = myPlayer.mediainfo.link.url,
-      hrefString =
-        '<a href = "' + linkURL + '" target="_blank">' + linkText + "</a>";
-    
-    // +++ Inject anchor tag into HTML +++
-    document.getElementById("linkHere").innerHTML = hrefString;
+    cuePointAra = myPlayer.mediainfo.cue_points;
+    var tt = myPlayer.textTracks()[0];
+    // When a cue point starts, extract cue data and pass data to be displayed
+    tt.oncuechange = function() {
+      if (tt.activeCues[0] !== undefined) {
+        allCuePointData = getSubArray(
+          cuePointAra,
+          "time",
+          tt.activeCues[0].startTime
+        );
+        displayCTA(allCuePointData[0]);
+      }
+    };
+    myPlayer.play();
   });
+
+  // +++ Extract data pieces from cue point meta data and display overlay  +++
+  function displayCTA(cpData) {
+    // Split metadata into an array of three pieces based on location of semicolons
+    var dataAra = cpData.metadata.split(";"),
+      durationCTA = dataAra[0],
+      textCTA = dataAra[1],
+      urlCTA = dataAra[2],
+      // Dynamically build the anchor tag
+      hrefCTA =
+        '<a href="' +
+        urlCTA +
+        '"><span style="background-color: black; color: red;">' +
+        textCTA +
+        "</span></a>",
+      timeoutValue;
+    // Set duration, set timeout, inject dynamic html and show overlay
+    timeoutValue = Number(durationCTA) * 1000;
+    timeoutID = window.setTimeout(checkTime, timeoutValue);
+    document.getElementsByClassName("vjs-overlay")[0].innerHTML = hrefCTA;
+    myPlayer.removeClass("hide-overlay");
+  }
+
+  // +++ Clear timeout and remove overlay +++
+  // This function called when timeout is reached
+  function checkTime() {
+    window.clearTimeout(timeoutID);
+    myPlayer.addClass("hide-overlay");
+  }
+
+  // +++ Helper function to extract cue point info +++
+  function getSubArray(targetArray, objProperty, value) {
+    var i,
+      totalItems = targetArray.length,
+      objFound = false,
+      idxArr = [];
+    for (i = 0; i < totalItems; i++) {
+      if (targetArray[i][objProperty] === value) {
+        objFound = true;
+        idxArr.push(targetArray[i]);
+      }
+    }
+    return idxArr;
+  }
 });
